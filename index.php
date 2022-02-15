39ede3bf103d63b94829cc1958fa306018b37ab7
farmanhaider123 <97297440+farmanhaider123@users.noreply.github.com>
2022-02-16 02:06:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 <html lang="en">
 <head>
 <style>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="utf-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </style>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
